a06ba91bc7a01a4f78cdcc098bc6c83b3c40c8b0
Phoenix <drake.mcgarrah@gmail.com>
2020-02-05 11:21:49 -0600

    Started home.html and home.js

---
+++ public/js/home.js
@@ -0,0 +1,10 @@
+$("#searchBtn").on("click", function (e) {
+    e.preventDefault();
+    const civicURL = "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyCWvaHq1bAZ111hZ4qrZd6pdazM9LBEBcc";
+    $.ajax({
+        url: civicURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+});
