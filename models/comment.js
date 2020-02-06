409248dfc30c84cd9ac555f3eb09c3589f9ec27e
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-02-06 11:21:24 -0600

    renamed model files, creted new tables

--- models/comment.js
+++ models/comment.js
@@ -1,30 +1,16 @@
-// Create the Comment model
-module.exports = function (sequelize, DataTypes) {
-    var Comment = sequelize.define("Comment", {
-        // Create a column for the comment text. The text cannot be null or blank.
-        text: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        }
-    });
+module.exports = function(sequelize, DataTypes) {
+  var Comment = sequelize.define("Comment", {
+    type: DataTypes.STRING,
+    allowNull: true
+  });
 
-    // Associate comments
-    Comment.associate = function (models) {
-        // A Comment must belong to a User and an Official
-        // A Comment can't be created without a User and an Official due to the foreign key not allowing null. 
-        Comment.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-        Comment.belongsTo(models.Official, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    return Comment;
+  Comment.associate = function(models) {
+    Comment.belongsTo(models.Person, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Comment;
 };
--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,16 @@
-$("#searchBtn").on("click", function (e) {
-    e.preventDefault();
-    const zip = $("#zip").val().trim();
-    const repURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCWvaHq1bAZ111hZ4qrZd6pdazM9LBEBcc&address=" + zip;
+$("#searchBtn").on("click", function(e) {
+  e.preventDefault();
+  const zip = $("#zip")
+    .val()
+    .trim();
+  const repURL =
+    "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCWvaHq1bAZ111hZ4qrZd6pdazM9LBEBcc&address=" +
+    zip;
 
-    $.ajax({
-        url: repURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
+  $.ajax({
+    url: repURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
 });
